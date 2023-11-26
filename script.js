252a15cf575a47a63ad405f9a91eeedad92319e2
Brooks Gunn <brooks.a.gunn@gmail.com>
2023-11-26 11:44:40 -0500

    Add show/hide toggle for individual faqs onClick

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const faqBlock = document.getElementById("faq-block");
-const answers = document.querySelectorAll(".answer");
 
 let faqArr = [
-  { question: "Question 1", answer: "Answer 1" },
-  { question: "Question 2", answer: "Answer 2" },
+  {
+    question: "Why is there no light in space?",
+    answer:
+      "Space appears dark because there is little to no matter for light to bounce back into our line of sight. Space is a near-perfect vacuum, so there is almost nothing to scatter light between stars and planets.",
+  },
+  {
+    question: "Where did the renaissance begin?",
+    answer:
+      'The Renaissance is generally considered to have begun in Florence, Italy between 1350 and 1400. The Renaissance was a cultural movement that spread throughout Europe and ended around 1600. The word "Renaissance" means "rebirth".',
+  },
   { question: "Question 3", answer: "Answer 3" },
 ];
 
@@ -13,6 +20,20 @@ faqArr.forEach((faq) => {
         <h2>${faq.question}</h2>
         <i class="fa-solid fa-chevron-down" style="color: #095cec"></i>
     </div>
-    <div class="answer">${faq.answer}</div>
+    <div class="answer" style="display: none">${faq.answer}</div>
 </div>`;
+});
+
+const toggleDisplay = (el) => {
+  el.style.display === "none"
+    ? (el.style.display = "block")
+    : (el.style.display = "none");
+};
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-chevron-down")) {
+    let faqParent = e.target.parentNode;
+    let corrAnswer = faqParent.nextElementSibling;
+    toggleDisplay(corrAnswer);
+  }
 });
